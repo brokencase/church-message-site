b9775b5480b95eb387302e565a7652f40b6321ec
meridioscase <73130749+meridioscase@users.noreply.github.com>
2020-11-05 06:55:35 -0500

    Resource and Login Pages
    Added resource book page and the beginning of the login page plus routes.

--- app.py
+++ app.py
@@ -9,5 +9,13 @@ def index():
 def contact_page():
     return render_template('contact.html')
 
+@app.route('/parenting-book')
+def book_page():
+    return render_template('resource-parenting.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('user-login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
